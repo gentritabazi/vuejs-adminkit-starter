db026a99aed3c84ed212af30059fc0fd70685154
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-15 11:19:45 +0200

    Add App

---
+++ src/modules/App/App.js
@@ -0,0 +1,14 @@
+import loaderComponent from '@/components/Loader'
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'App',
+    components: {
+        loaderComponent
+    },
+    computed: {
+        ...mapGetters({
+            displayLoader: 'getDisplayLoader'
+        })
+    }
+}
